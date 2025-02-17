d17aebcd716d97f7892b9093f4c6c2580f37fad3
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2025-02-17 11:51:13 -0500

    Metadata & Start loading

--- skilltreegodot/Scripts/SkillTreeTool.gd
+++ skilltreegodot/Scripts/SkillTreeTool.gd
@@ -13,8 +13,9 @@ extends Control
 
 @export var LoadSpreadsheetData: bool:
 	set(value):
-		pass
-		#WriteFile(sampleData)
+		DataLoad()
+
+@export var ids : Array[int]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,6 +39,11 @@ func ToolsetGeneration():
 	WriteFile(data)
 
 
+func DataLoad():
+	for tree in trees:
+		RecurLoadData(tree)
+
+# Generates a spreadsheet based on the btn hierarchies 
 func RecurGenerateData(curr):
 	
 	for child in curr.get_children():
@@ -47,15 +53,23 @@ func RecurGenerateData(curr):
 			
 				# Generate GUID
 				# Note that we are not registering to a resource
-				# since we are managing our nodes outside of GODOT 
-				var id = ResourceUID.create_id()
-				var unlocked = false 
-				var abilityName = "abilityName"
-				var laborCount = 0
-				var laborMax = 1
-				var abilityDescription = "abilityDescription"
+				# since we are managing our data outside of GODOT 
+				var id = ResourceUID.create_id() / 10000
 				
-				nodeData.push_back(str(id / 10000))
+				# TODO: Optimize this check 
+				while ids.has(id):
+					id = ResourceUID.create_id() / 10000
+				ids.push_back(id)
+				child.set_meta("id", id)
+				
+				# Uses metadata of btn is avaliable otherwise adds it manually 
+				var unlocked = child.get_meta("Unlocked", false)
+				var abilityName = child.get_meta("AbilityName", "abilityName") 
+				var laborCount = child.get_meta("LaborCount", 0) 
+				var laborMax = child.get_meta("LaborMax", 1) 
+				var abilityDescription = child.get_meta("Description", "abilityDescription") 
+				
+				nodeData.push_back(str(id))
 				nodeData.push_back(str(unlocked))
 				nodeData.push_back(abilityName)
 				nodeData.push_back(str(laborCount))
@@ -70,12 +84,26 @@ func RecurGenerateData(curr):
 	return ""
 
 
+func RecurLoadData(curr):
+	
+	# Load in spreadsheet data 
+	# Iterate through each child of tree
+	#	Search for ID in spreadsheet data
+	#	if ID is present set data appropriatly 
+	#		Remove ID for spreadsheet data list 
+	ReadFile()
+	pass 
+
 func WriteFile(content):
 	var file = FileAccess.open("res://SkillTreeData/Sample.csv", FileAccess.WRITE)
 	file.store_string(content)
 	#file.store_csv_line(content)
 
 func ReadFile():
+	
+	var data : Array
 	var file = FileAccess.open("res://SkillTreeData/Sample.csv", FileAccess.READ)
 	while !file.eof_reached():
 		var content = file.get_csv_line()
+		data.push_back(content)
+	
